6c54bad86ff4425da0a212e11a14bf7cd6486e26
quuee <bandzuangon@gmail.com>
2024-04-05 21:31:09 +0800

    up

--- src/main/java/cn/quuee/chatServer/model/IMLoginInfo.java
+++ src/main/java/cn/quuee/chatServer/model/IMLoginInfo.java
@@ -1,9 +1,9 @@
-package cn.quuee.chatServer.model;
-
-import lombok.Data;
-
-@Data
-public class IMLoginInfo {
-
-    private String accessToken;
-}
+//package cn.quuee.chatServer.model;
+//
+//import lombok.Data;
+//
+//@Data
+//public class IMLoginInfo {
+//
+//    private String accessToken;
+//}
